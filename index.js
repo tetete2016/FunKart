8c9b3c66794c45da177477794e732eaf87898e17
YOUR NAME <tetete@iMac.local>
2017-04-01 17:23:48 +0900

    added files for node

---
+++ index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static('public'));
+app.get('/', function (request, response) {
+    response.send("error");
+});
+app.listen(app.get('port'), function () {
+    console.log('Node app is running on port', app.get('port'));
+});
